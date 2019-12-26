012c0d0bb687895fab1257fde3eb4ba7593b0f1f
linan <linan@wtoip.com>
2019-12-26 16:37:04 +0800

    MQ操作简单实例

--- src/main/java/com/huoxin4415/aje/rabbitmq/SimpleStarter.java
+++ src/main/java/com/huoxin4415/aje/rabbitmq/SimpleStarter.java
@@ -11,14 +11,14 @@ import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
 /**
- * 连接MQ实例
- * 1.创建ConnectionFactory
- * 2.创建Connection&channel
+ * 连接MQ实例 
+ * 1.创建ConnectionFactory 
+ * 2.创建Connection&channel 
  * 3.声明exchange&queue
- * 4.publish message
+ * 4.publish message 
  * 5.consume message
  */
-public class ConnectToMQ {
+public class SimpleStarter {
 
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
@@ -34,32 +34,40 @@ public class ConnectToMQ {
         Connection conn = factory.newConnection();
         Channel channel = conn.createChannel();
 
-        final String exchangeName = "amq.direct";
-        final String routingKey = "key.hello";
+        final String exchangeName = "amq.topic";
+        final String routingKey = "topic.test";
 
         // 创建exchange，默认amq.direct已存在，不再创建
         // channel.exchangeDeclare(exchangeName, "direct", true);
 
         String queueName = channel.queueDeclare().getQueue();
-        // 创建临时、排他队列
-        channel.queueBind(queueName, exchangeName, routingKey);
+        // 创建临时&排他队列
+        // channel.queueBind(queueName, exchangeName, routingKey);
 
         // publish message
         byte[] messageBodyBytes = "Hello, world!".getBytes();
-        channel.basicPublish(exchangeName, routingKey, null, messageBodyBytes);
+        channel.basicPublish(exchangeName, routingKey, 
+                new AMQP.BasicProperties.Builder().type("example.hello").build(),   // set properties
+                messageBodyBytes);
         System.out.println("publish message:" + new String(messageBodyBytes));
 
         // consume message
         boolean autoAck = false;
-        channel.basicConsume(queueName, autoAck, "myConsumerTag", new DefaultConsumer(channel) {
+        String consumerTag = channel.basicConsume(queueName, autoAck, "myConsumerTag", new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
                     byte[] body) throws IOException {
                 long deliveryTag = envelope.getDeliveryTag();
                 System.out.println("consume message:" + new String(body));
+                System.out.println("properties type:" + properties.getType()); // get properties
                 channel.basicAck(deliveryTag, true);
             }
         });
+
+        System.out.println("Click anything to close...");
+        System.in.read();
+
+        channel.basicCancel(consumerTag);   // cancel consumer
 
         channel.close();
         conn.close();
